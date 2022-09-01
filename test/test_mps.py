3baf2b84400adaa5223c2b620eab3c5f9208db3f
Abhishek Pathak <abhipathak97@gmail.com>
2022-09-01 11:31:36 -0700

    Exclude split from tests (#101)

--- test/test_mps.py
+++ test/test_mps.py
@@ -7920,6 +7920,7 @@ class TestConsistency(TestCase):
         'repeat': ['torch.bool'],
         'rot90': ['torch.bool'],
         'tile': ['torch.bool'],
+        'split': ['torch.float32'],
     }
 
     # Used for accept mode only
