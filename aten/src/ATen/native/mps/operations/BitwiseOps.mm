1cf2d44112b6403e0dcb70d213cd0464253c9158
Denis Vieriu <104024078+DenisVieriu97@users.noreply.github.com>
2022-10-05 23:08:15 -0700

    Fix empty dispatch for bitwise_not (#132)

--- aten/src/ATen/native/mps/operations/BitwiseOps.mm
+++ aten/src/ATen/native/mps/operations/BitwiseOps.mm
@@ -310,6 +310,10 @@ at::Tensor& bitwise_not_out_mps (const at::Tensor& self, at::Tensor& output_) {
                                                      getMetalType(self),
                                                      "bitwise_not");
   uint32_t length = output.numel();
+  if (length == 0) {
+    return output_;
+  }
+
   dispatch_sync(stream->queue(), ^(){
     id<MTLCommandBuffer> buffer = stream->commandBuffer();
     id<MTLComputeCommandEncoder> commandEncoder = [buffer computeCommandEncoder];
