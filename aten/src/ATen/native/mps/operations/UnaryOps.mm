ab4394667964fca50fa478ccdb8411cb77efed34
Ramin Azarmehr <razarmehr@apple.com>
2022-10-26 18:59:03 -0400

    Fix data type issues with log1p() op (#151)

--- aten/src/ATen/native/mps/operations/UnaryOps.mm
+++ aten/src/ATen/native/mps/operations/UnaryOps.mm
@@ -188,49 +188,21 @@ Tensor& logical_not_out_mps(const Tensor& self, Tensor& output)
 
 TORCH_IMPL_FUNC(log1p_out_mps) (const Tensor& self, const Tensor& output)
 {
-    TORCH_CHECK(self.scalar_type() != ScalarType::Long, "MPS does not support log1p op with int64 input")
-    using namespace mps;
-    if (!output.is_same_size(self)) {
-      output.resize_(self.sizes());
-    }
-    MPSGraphCache* cache_ = MPSGraphCache::getInstance();
-    @autoreleasepool {
-      string key = string("log1p_out_mps") + getTensorsStringKey({self});
-      auto cachedGraph = cache_->LookUpAs<MPSUnaryCachedGraph>(key);
-
-      if(!cachedGraph) {
-        cachedGraph = cache_->CreateCachedGraphAs<MPSUnaryCachedGraph>(key, ^ MPSCachedGraph* () {
-          MPSUnaryCachedGraph *newCachedGraph = nil;
-          @autoreleasepool {
-            MPSGraph* mpsGraph = make_mps_graph();
-            newCachedGraph = new MPSUnaryCachedGraph(mpsGraph);
-            newCachedGraph->inputTensor_ = mpsGraphRankedPlaceHolder(mpsGraph, self);
-              MPSGraphTensor* oneTensor = [mpsGraph constantWithScalar:1.0
-                                                          shape:getMPSShape(self)
-                                                       dataType:mps::getMPSDataType(self.scalar_type())];
-              MPSGraphTensor* addedTensor = [mpsGraph additionWithPrimaryTensor:newCachedGraph->inputTensor_
-                                                         secondaryTensor:oneTensor
-                                                                    name:nil];
-            newCachedGraph->outputTensor_ = [mpsGraph logarithmWithTensor:addedTensor
-                                                                    name:nil];
-          }
-          return newCachedGraph;
-        });
-      }
-
-      Placeholder selfPlaceholder = Placeholder(cachedGraph->inputTensor_, self);
-      Placeholder outputPlaceholder = Placeholder(cachedGraph->outputTensor_, output);
-      NSDictionary<MPSGraphTensor*, MPSGraphTensorData*>* feeds = @{
-        selfPlaceholder.getMPSGraphTensor() : selfPlaceholder.getMPSGraphTensorData()
-      };
-      NSDictionary<MPSGraphTensor*, MPSGraphTensorData*>* results = @{
-        outputPlaceholder.getMPSGraphTensor() : outputPlaceholder.getMPSGraphTensorData()
-      };
-      runMPSGraph(getCurrentMPSStream(), cachedGraph->graph(), feeds, results);
-    }
+  TORCH_CHECK(self.scalar_type() != ScalarType::Long, "MPS does not support log1p op with int64 input");
+  mps::unary_op(self, output, "log1p_out_mps",
+                ^ MPSGraphTensor* (MPSGraph* mpsGraph, MPSGraphTensor* inputTensor) {
+                  MPSGraphTensor* oneTensor = [mpsGraph constantWithScalar:1.0
+                                                                  dataType:inputTensor.dataType];
+                  MPSGraphTensor* addedTensor = [mpsGraph additionWithPrimaryTensor:inputTensor
+                                                                    secondaryTensor:oneTensor
+                                                                               name:nil];
+                  return [mpsGraph logarithmWithTensor:addedTensor
+                                                  name:nil];
+                });
 }
 
-TORCH_IMPL_FUNC(frac_out_mps) (const Tensor& self, const Tensor& output) {
+TORCH_IMPL_FUNC(frac_out_mps) (const Tensor& self, const Tensor& output)
+{
   TORCH_CHECK(isFloatingType(self.scalar_type()), "frac_out_mps is only implemented for floating types");
   mps::unary_op(self, output, "frac_out_mps",
                 ^ MPSGraphTensor* (MPSGraph* mpsGraph, MPSGraphTensor* inputTensor) {
