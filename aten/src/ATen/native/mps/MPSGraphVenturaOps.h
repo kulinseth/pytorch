ebc38d06524e2791d5d299dc61f00d894018566b
Kulin Seth <kulin_seth@apple.com>
2022-12-09 16:16:32 -0800

    Build fixes.

--- aten/src/ATen/native/mps/MPSGraphVenturaOps.h
+++ aten/src/ATen/native/mps/MPSGraphVenturaOps.h
@@ -3,6 +3,20 @@
 
 // TODO: Remove me when moved to MacOS 13
 @interface MPSGraph (VenturaOps)
+API_AVAILABLE(macos(13.0))
+typedef NS_ENUM(NSUInteger, MPSGraphResizeNearestRoundingMode)
+{
+    MPSGraphResizeNearestRoundingModeRoundPreferCeil   =  0L,
+    MPSGraphResizeNearestRoundingModeRoundPreferFloor  =  1L,
+    MPSGraphResizeNearestRoundingModeCeil              =  2L,
+    MPSGraphResizeNearestRoundingModeFloor             =  3L,
+    MPSGraphResizeNearestRoundingModeRoundToEven       =  4L,
+    MPSGraphResizeNearestRoundingModeRoundToOdd        =  5L,
+};
+
+- (MPSGraphTensor *)cumulativeSumWithTensor:(MPSGraphTensor *)tensor
+                                       axis:(NSInteger)axis
+                                       name:(NSString *)name;
 
 #if !defined(__MAC_13_0) && \
     (!defined(MAC_OS_X_VERSION_13_0) || (MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_13_0))
