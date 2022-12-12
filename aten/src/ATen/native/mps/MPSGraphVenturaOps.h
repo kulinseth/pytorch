a2d29e9e8d41732e8b2b4cca6e6d18fdd01d3aa0
Denis Vieriu <dvieriu@apple.com>
2022-12-12 12:11:16 -0800

    Update macos macro check for MPSGraphResizeNearestRoundingMode

--- aten/src/ATen/native/mps/MPSGraphVenturaOps.h
+++ aten/src/ATen/native/mps/MPSGraphVenturaOps.h
@@ -4,7 +4,7 @@
 // TODO: Remove me when moved to MacOS 13
 @interface MPSGraph (VenturaOps)
 
-#if !defined(__MAC_10_13)
+#if !defined(__MAC_13_0)
 API_AVAILABLE(macos(13.0))
 typedef NS_ENUM(NSUInteger, MPSGraphResizeNearestRoundingMode)
 {
