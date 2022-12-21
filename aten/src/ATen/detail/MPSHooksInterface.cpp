e6c405f01e6efbae9a0d3105f8d025be40b29a64
Ramin Azarmehr <razarmehr@apple.com>
2022-12-21 17:37:09 +0000

    [MPS] Add MPSHooks interface to enable accessing MPS functions globally (#91104)
    This PR is a prerequisite to the upcoming MPSGenerator changes required for Random Ops.
    
    Add `MPSHooksInterface.cpp` to `aten_cpu_source_non_codegen_list`
    
    Co-authored-by: Nikita Shulga <nikita.shulga@gmail.com>
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/91104
    Approved by: https://github.com/kulinseth, https://github.com/malfet

--- aten/src/ATen/detail/MPSHooksInterface.cpp
+++ aten/src/ATen/detail/MPSHooksInterface.cpp
@@ -1,3 +1,5 @@
+//  Copyright © 2022 Apple Inc.
+
 #include <ATen/detail/MPSHooksInterface.h>
 #include <c10/util/Exception.h>
 #include <c10/util/CallOnce.h>
@@ -12,16 +14,12 @@ const MPSHooksInterface& getMPSHooks() {
   c10::call_once(once, [] {
     mps_hooks = MPSHooksRegistry()->Create("MPSHooks", MPSHooksArgs{});
     if (!mps_hooks) {
-      mps_hooks =
-          // NOLINTNEXTLINE(modernize-make-unique)
-          std::unique_ptr<MPSHooksInterface>(new MPSHooksInterface());
+      mps_hooks = std::make_unique<MPSHooksInterface>();
     }
   });
 #else
   if (mps_hooks == nullptr) {
-    mps_hooks =
-        // NOLINTNEXTLINE(modernize-make-unique)
-        std::unique_ptr<MPSHooksInterface>(new MPSHooksInterface());
+    mps_hooks = std::make_unique<MPSHooksInterface>();
   }
 #endif
   return *mps_hooks;
