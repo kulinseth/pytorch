dabe321da1b0f5ba191bd27d9bfa901c1c7d94ed
Denis Vieriu <104024078+DenisVieriu97@users.noreply.github.com>
2023-01-09 19:23:19 +0200

    Fix min/max_reduction_with_dim ops (#224)

--- test/test_mps.py
+++ test/test_mps.py
@@ -8401,6 +8401,8 @@ class TestConsistency(TestCase):
         'masked.std': ['b8', 'f16', 'f32', 'i16', 'i32', 'i64', 'u8'],
         'masked.sum': ['b8', 'f16', 'f32', 'i16', 'i32', 'i64', 'u8'],
         'masked.var': ['b8', 'f32', 'i16', 'i32', 'i64', 'u8'],
+        'minreduction_with_dim': ['b8', 'f16', 'f32', 'i16', 'i32', 'i64', 'u8'],
+        'maxreduction_with_dim': ['b8', 'f16', 'f32', 'i16', 'i32', 'i64', 'u8'],
     }
 
 
@@ -8594,8 +8596,7 @@ class TestConsistency(TestCase):
         'divfloor_rounding': [torch.int16, torch.int32, torch.int64],
         'divtrunc_rounding': [torch.float16],
         'norm': [torch.float16],
-        'minreduction_with_dim': [torch.bool, torch.int16, torch.uint8],
-        'maxreduction_with_dim': [torch.bool, torch.int16, torch.uint8],
+
         'square': [torch.bool, torch.int16, torch.int32, torch.int64, torch.uint8],
         # Functions that are flaky
         # These are detected as "ok" by the expect case but actually fail to run sometimes
