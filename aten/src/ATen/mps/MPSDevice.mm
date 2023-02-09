c99459d6d784c5da4066f116907e2813f7dc6361
skotapati <siddharth.kotapati@gmail.com>
2023-02-09 12:01:06 -0800

    Added zero check to inverse & fix for any op to avoid segfault issue (#298)
    * Added zero check to inverse & fix for any op to avoid segfault issue
    
    * Added macOS version check for enabling inverse
    
    * Using @available to determine MPSGraph version
    
    * Fixed error due to incorrect usage of @available
    
    ---------
    
    Co-authored-by: sidk <sidk@sidks-MacBook-Pro.local>

--- aten/src/ATen/mps/MPSDevice.mm
+++ aten/src/ATen/mps/MPSDevice.mm
@@ -98,11 +98,15 @@ bool MPSDevice::isMacOS13Plus(int32_t subVersion) const {
   static bool _macos_13_1_plus = [mpsCD instancesRespondToSelector:@selector(
     sampleGridWithSourceTensor:coordinateTensor:layout:normalizeCoordinates:relativeCoordinates:alignCorners:paddingMode:samplingMode:constantValue:name:)] == YES;
   static bool _macos_13_2_plus = [mpsCD instancesRespondToSelector:@selector(convolution3DWithSourceTensor:weightsTensor:descriptor:name:)] == YES;
+  static bool _macos_13_3_plus = NO;
+  if (@available(macOS 13.3, *))
+    _macos_13_3_plus = YES;
 
   switch (subVersion) {
     case 0:  return _macos_13_0_plus;
     case 1:  return _macos_13_1_plus;
     case 2:  return _macos_13_2_plus;
+    case 3:  return _macos_13_3_plus;
     default: return false;
   }
 }
--- test/test_mps.py
+++ test/test_mps.py
@@ -10052,8 +10052,6 @@ class TestConsistency(TestCaseMPS):
     # All the entries in this list should be removed
     BLOCKLIST = {
         # Functions that hard crash
-        'linalg.inv': [torch.float32],
-        'linalg.inv_ex': [torch.float32],
         'linalg.matrix_power': [torch.float32],
         'resize_': [torch.bool, torch.float16, torch.float32, torch.int16, torch.int32, torch.int64, torch.uint8],
         'resize_as_': [torch.float16, torch.float32],
