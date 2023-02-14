9cc57dd488a1cc0596cc97c31b2eb10d82fbaf6a
Kulin Seth <kulin_seth@apple.com>
2023-02-14 08:21:06 -0800

    Update the test mps.
    Remove torch._six from test_mps (#326)
    
    Fix test_zero_grad() (#330)
    
    Fix bilinear backward pass (#331)
    
    * Fix bilinear backward pass
    
    * Remove comment
    
    Update macOS 12 blocklist (#323)
    
    * Update macOS 12 blocklist
    - move sum, masked.var, mul to low precision list
    - unblock them from running
    
    * - mark __rdiv__ failures as accumulate error exceeds atol/rtol
    
    Fix nn.functional.embedding grad (#335)
    
    - casting the input tensor to float32 and cast back the output tensor
    - unblock the test
    
    Fix prelu backward (#334)
    
    Reduction cast f16 to f32 only on macOS 12 (#332)
    
    - unblock rdiv float16
    
    Fix trace op (#340)
    
    - give warnings of converting int64 for reduction ops
    - use cast tensor for reduction sum on trace
    - unblock trace from running
    
    Update random result list (#339)
    
    * - move nn.functional.feature_alpha_dropoutwith_train, normalnumber_mean, new_empty_strided to expected failures
    
    * - update new_empty_strided
    
    ---------
    
    Co-authored-by: Kulin Seth <kulin_seth@apple.com>
    
    Enable int8 in TestConsistency (#347)
    
    Dev/skotapati/copy broadcasting (#350)
    
    * Handle broadcasting by expanding src tensor in Copy.mm
    
    * Unblock linalg_matrix_power
    
    * Improved formatting
    
    Add the functionality to dump MPS ops.
    
    1. DUMP_MPS_OPS to use LoggingTensor to dump out the ATen ops.
    2. Skip running the EXPECTTEST list, as some tests are still
       seg-faulting
    
    Fix lintrunner errors (#353)
    
    * Fix lintrunner errors
    
    * - move normal_in_place to random result list
    
    Fixed the test_mps.
    
    Test mps is updated.

---
+++ test/test_mps_utils.py
@@ -0,0 +1,103 @@
+import torch
+from torch.testing._internal.common_utils import TestCase, run_tests
+from torch.utils._pytree import tree_map
+
+from typing import Iterator, List
+import logging
+import contextlib
+import itertools
+
+class LoggingTensor(torch.Tensor):
+    elem: torch.Tensor
+
+    __slots__ = ['elem']
+
+    @staticmethod
+    def __new__(cls, elem, *args, **kwargs):
+        # The wrapping tensor (LoggingTensor) shouldn't hold any
+        # memory for the class in question, but it should still
+        # advertise the same device as before
+        r = torch.Tensor._make_wrapper_subclass(
+            cls, elem.size(),
+            # TODO: clone strides and storage aliasing
+            dtype=elem.dtype, layout=elem.layout,
+            device=elem.device, requires_grad=elem.requires_grad
+        )
+        # ...the real tensor is held as an element on the tensor.
+        r.elem = elem
+        return r
+
+    def __repr__(self):
+        return f"LoggingTensor({self.elem})"
+
+    @classmethod
+    def __torch_dispatch__(cls, func, types, args=(), kwargs=None):
+        def unwrap(e):
+            return e.elem if isinstance(e, LoggingTensor) else e
+
+        def wrap(e):
+            return LoggingTensor(e) if isinstance(e, torch.Tensor) else e
+
+        rs = tree_map(wrap, func(*tree_map(unwrap, args), **tree_map(unwrap, kwargs)))
+        logging.getLogger("LoggingTensor").info(f"{func.__module__}.{func.__name__}", args, kwargs, rs)
+        return rs
+
+# https://stackoverflow.com/questions/36408496/python-logging-handler-to-append-to-list
+class LoggingTensorHandler(logging.Handler):
+
+    def __init__(self, log_list) -> None:
+        logging.Handler.__init__(self)
+        self.log_list = log_list
+        self.next_shortid = 0
+
+    # WARNING: not deterministic over multiple threads, this matters for
+    # autograd
+    def _shortid(self, o: object) -> int:
+        if not hasattr(o, '_shortid'):
+            o._shortid = self.next_shortid
+            self.next_shortid += 1
+        return o._shortid
+
+    def _fmt(self, a: object) -> str:
+        if isinstance(a, LoggingTensor):
+            return f'${self._shortid(a)}'
+        elif isinstance(a, torch.nn.Parameter):
+            return f'Parameter(..., size={tuple(a.size())})'
+        elif isinstance(a, torch.Tensor):
+            return f'Tensor(..., size={tuple(a.size())})'
+        else:
+            return repr(a)
+
+    def emit(self, record):
+        fmt_args = ", ".join(itertools.chain(
+            (self._fmt(a) for a in record.args[0]),
+            (f"{k}={self._fmt(v)}" for k, v in record.args[1].items())
+        ))
+        fmt_rets = ", ".join(self._fmt(a) for a in record.args[2]) \
+            if isinstance(record.args[2], (list, tuple)) else self._fmt(record.args[2])
+        self.log_list.append(f'{fmt_rets} = {record.msg}({fmt_args})')
+
+@contextlib.contextmanager
+def capture_logs():
+    logger = logging.getLogger("LoggingTensor")
+    log_list = []
+    handler = LoggingTensorHandler(log_list)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    try:
+        yield log_list
+    finally:
+        logger.removeHandler(handler)
+
+def tracefunc(frame, event, arg, indent=[0]):
+      if event == "call":
+          indent[0] += 2
+          print("-" * indent[0] + "> call function", frame.f_code.co_name)
+      elif event == "return":
+          print("<" + "-" * indent[0], "exit function", frame.f_code.co_name)
+          indent[0] -= 2
+      return tracefunc
+
+import sys
+
