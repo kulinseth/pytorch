3ae312499ae3b9480c6279e4bbb1c7bd7638d7f8
Denis Vieriu <dvieriu@apple.com>
2023-03-01 14:55:57 -0800

    Cast int64 to float32 for older OSes than 13.3

--- test/test_mps.py
+++ test/test_mps.py
@@ -2741,7 +2741,7 @@ class TestMPS(TestCaseMPS):
             helper(torch.int64)
         except Exception as e:
             e_string = str(e)
-            self.assertEqual(e_string, "MPS does not support cumsum op with int64 input")
+            self.assertEqual(e_string, "MPS does not support cumsum op with int64 input. Support has been added in macOS 13.3")
 
     def test_gelu_tanh(self):
         def helper(shape):
