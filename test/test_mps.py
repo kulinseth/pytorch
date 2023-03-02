6a75dcbaa73e854d9cf5b8345b6a26770381206c
skotapati <skotapati@apple.com>
2023-03-02 10:03:12 -0800

    Add parentheses to atanh grad to ensure correct output

--- test/test_mps.py
+++ test/test_mps.py
@@ -10599,9 +10599,6 @@ class TestConsistency(TestCaseMPS):
         'unfold': ['f16', 'f32'],
         'trace': ['f32'],
 
-        # Correctness issues
-        'atanh': ['f32'],
-
         # Unsupported dtype
         'special.ndtr': ['f32'],
         'trapezoid': ['f16', 'f32'],
