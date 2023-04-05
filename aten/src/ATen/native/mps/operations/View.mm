131ccc64a2e50a4b87154cb447a30fcb56db3186
Ramin Azarmehr <razarmehr@apple.com>
2023-04-05 12:22:41 -0400

    Enable Adaptive Commit for kernel calls (#411)
    * Enable Adaptive Commit for kernel calls
    
    * Add output tensor to commitAdaptive

--- aten/src/ATen/native/mps/operations/View.mm
+++ aten/src/ATen/native/mps/operations/View.mm
@@ -865,8 +865,7 @@ Tensor gatherViewTensor(const at::Tensor& src, at::Tensor& dst) {
 
     MTLSize threadsPerThreadgroup = MTLSizeMake(threadsPerThreadgroup_, 1, 1);
     [computeEncoder dispatchThreads:gridSize threadsPerThreadgroup:threadsPerThreadgroup];
-
-    getMPSProfiler().endProfileKernel(gatherPSO);
+    mpsStream->commitAdaptive({src, output}, gatherPSO);
   });
 
   return (dst.has_storage()) ? dst : output;
@@ -928,8 +927,7 @@ Tensor& scatterViewTensor(const at::Tensor& src, at::Tensor& output){
 
       MTLSize threadsPerThreadgroup = MTLSizeMake(threadsPerThreadgroup_, 1, 1);
       [computeEncoder dispatchThreads:gridSize threadsPerThreadgroup:threadsPerThreadgroup];
-
-      getMPSProfiler().endProfileKernel(scatterPSO);
+      mpsStream->commitAdaptive({src, output}, scatterPSO);
     }
   });
 
