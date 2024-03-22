4f4913f34e1ac7ea620990990b8aa85bf7f66c39
Joona Havukainen <jhavukainen@apple.com>
2024-03-21 17:10:11 -0700

    Indexing and softmax changes

--- aten/src/ATen/native/mps/operations/Indexing.mm
+++ aten/src/ATen/native/mps/operations/Indexing.mm
@@ -646,10 +646,10 @@ Tensor& index_select_out_mps(const Tensor& self, int64_t dim, const Tensor& inde
   }
 
   @autoreleasepool {
-    string key = "index_select_out_mps" + getTensorsStringKey({self, index}) + ":" + std::to_string(dim);
+    string key = "index_select_out_mps" + getTensorsStringKey({self, index}, true, /*exclude_shape*/true) + ":" + std::to_string(dim);
     auto cachedGraph = LookUpOrCreateCachedGraph<CachedGraph>(key, [&](auto mpsGraph, auto newCachedGraph) {
-      MPSGraphTensor* inputTensor = mpsGraphRankedPlaceHolder(mpsGraph, inputType, getMPSShape(self));
-      MPSGraphTensor* indexTensor = mpsGraphRankedPlaceHolder(mpsGraph, index);
+      MPSGraphTensor* inputTensor = mpsGraphUnrankedPlaceHolder(mpsGraph, inputType);
+      MPSGraphTensor* indexTensor = mpsGraphUnrankedPlaceHolder(mpsGraph, getMPSDataType(index.scalar_type()));
 
       MPSGraphTensor* outputTensor = [mpsGraph gatherWithUpdatesTensor:inputTensor
                                                          indicesTensor:indexTensor
@@ -675,7 +675,11 @@ Tensor& index_select_out_mps(const Tensor& self, int64_t dim, const Tensor& inde
                                                 /*dataType=*/outputType);
 
     auto feeds = dictionaryFromPlaceholders(selfPlaceholder, indexPlaceholder);
-    runMPSGraph(stream, cachedGraph->graph(), feeds, outputPlaceholder);
+    NSDictionary<MPSGraphTensor*, MPSGraphTensorData*>* results = @{
+      outputPlaceholder.getMPSGraphTensor() : outputPlaceholder.getMPSGraphTensorData()
+    };
+
+    runMPSGraph(stream, cachedGraph, feeds, results, /*disable_type_inference=*/true);
   }
 
   return output;
